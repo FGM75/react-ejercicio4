09809cc975327ff1c84de688c3d1b3142f3d1c7f
FGM75 <fgonzalezm75@gmail.com>
2021-06-15 14:00:46 +0200

    formulatio html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 function App() {
-  return <p>Mirame Mama! he añadido Bootstrap.</p>;
+  return (
+    <form>
+      <label>
+        Nombre:<input id="nombre"></input>
+      </label>
+      <label>
+        apellido:<input id="apellido"></input>
+      </label>
+      <label>
+        Fecha de nacimiento:<input id="fecha nacimiento"></input>
+      </label>
+      <label>
+        Correo electronico:<input id="correo electronico"></input>
+      </label>
+    </form>
+  );
 }
 
 export default App;
